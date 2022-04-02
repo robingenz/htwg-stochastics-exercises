0d13800690eb1031c10678708cafd957902a23cf
Robin Genz <mail@robingenz.dev>
2022-04-02 09:09:15 +0000

    Add exercise `1.4`

---
+++ exercises/exercide_sheet_1/1_4.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def median(values: list) -> int:
+    return np.median(values)
+
+
+def main():
+    values = [5, 10, 20, 50, 100, 200, 500]
+    print(f"Median: {median(values)}")
+    values = [5, 10, 20, 50, 100, 200, 500, 1000]
+    print(f"Median (mit 1000€-Schein): {median(values)}")
+
+
+if __name__ == "__main__":
+    main()
