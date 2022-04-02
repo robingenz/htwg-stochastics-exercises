db72f26224aaf5631837640f61b7464e1049dd83
Robin Genz <mail@robingenz.dev>
2022-04-02 11:34:38 +0200

    Add exercise `1.5`

---
+++ exercises/exercide_sheet_1/1_5.py
@@ -0,0 +1,39 @@
+import numpy as np
+from scipy.stats import iqr
+
+
+def mean(values: list) -> int:
+    return np.mean(values)
+
+def median(values: list) -> int:
+    return np.median(values)
+
+def mode(values: list) -> int:
+    return max(set(values), key=values.count)
+
+def quantile(values: list, q: float) -> int:
+    return np.quantile(values, q)
+
+def interquartile_range(values: list) -> int:
+    return iqr(values)
+
+def span(values: list) -> int:
+    max_value = max(values)
+    min_value = min(values)
+    return max_value - min_value
+
+
+def main():
+    values = [2, 4, 3, 1, 2, 4, 2, 2, 2, 3]
+    print(f"Arithmetisches Mittel: {mean(values)}")
+    print(f"Median: {median(values)}")
+    print(f"Modalwert: {mode(values)}")
+    print(f"10%-Quantil: {quantile(values, 0.10)}")
+    print(f"25%-Quantil: {quantile(values, 0.25)}")
+    print(f"75%-Quantil: {quantile(values, 0.75)}")
+    print(f"Interquartilabstand: {interquartile_range(values)}")
+    print(f"Spannweite: {span(values)}")
+
+
+if __name__ == "__main__":
+    main()
