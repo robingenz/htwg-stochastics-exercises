678ab959f8bdd6ebefe304921f74231fc3201152
Robin Genz <mail@robingenz.dev>
2022-05-29 10:50:39 +0000

    style: remove comma

--- exercises/exercise_sheet_4/4_1_2.py
+++ exercises/exercise_sheet_4/4_1_2.py
@@ -16,7 +16,7 @@ def standard_deviation(values: list) -> int:
 
 
 def main():
-    values = [1, 1, 1, 1, 2, 3, 3, 3, ]
+    values = [1, 1, 1, 1, 2, 3, 3, 3]
     print(f"Erwartungswert: {expected_value(values)}")
     print(f"Varianz: {variance(values)}")
     print(f"Standardabweichung: {standard_deviation(values)}")
