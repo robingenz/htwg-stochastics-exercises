ae12368a834f397b2084e3f057eafc02e3d75cab
Robin Genz <mail@robingenz.dev>
2022-06-28 09:32:59 +0000

    Update temp file

--- _temp.py
+++ _temp.py
@@ -1,29 +1,19 @@
+# Source: https://www.w3schools.com/python/trypython.asp?filename=demo_matplotlib_scatter
+
+# Three lines to make our compiler able to draw:
 import numpy as np
-from statistics import covariance as cov
+import matplotlib.pyplot as plt
+import sys
+import matplotlib
+matplotlib.use('Agg')
 
 
-def mean(values: list) -> float:
-    return np.mean(values)
+x = np.array([25, 17, 25, 29, 20, 15, 11, 17, 16, 16])
+y = np.array([11, 11, 13, 9, 10, 7, 3, 8, 9, 4])
 
+plt.scatter(x, y)
+plt.show()
 
-def standard_deviation(values: list) -> float:
-    return np.std(values, ddof=1)
-
-
-def covariance(values1: list, values2: list) -> float:
-    return cov(values1, values2)
-
-
-def correlation_coefficient(values1: list, values2: list) -> float:
-    return covariance(values1, values2) / (standard_deviation(values1) * standard_deviation(values2))
-
-
-def main():
-    x1 = [28300, 28000, 41000, 40000, 48000, 47500,
-          43000, 50700, 50000, 48000, 25000, 24000]
-    x2 = [1.6, 2.6, 3.7, 5.3, 6.9, 7.1, 7.2, 6.7, 5.1, 3.6, 2.1, 1.4]
-    print(f"Korrelationskoeffizient: {correlation_coefficient(x1, x2)}")
-
-
-if __name__ == "__main__":
-    main()
+# Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
